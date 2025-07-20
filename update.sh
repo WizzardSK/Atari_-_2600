e3093e503a7484ff896928cb3a43662787da52fd
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2025-07-20 11:46:13 +0200

    Delete update.sh

--- update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-rm -f archive.zip
-zip -r archive.zip *
-git config --global user.name "GitHub Actions"
-git config --global user.email "actions@github.com"
-git add archive.zip
-git commit -m "Auto update ($(date +'%Y-%m-%d %H:%M:%S'))"
-git push
