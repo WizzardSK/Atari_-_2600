33f5e56fedc176b76c9d0a2623c579ad6f02295e
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2025-08-26 15:06:46 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -14,4 +14,4 @@ git add .
 git config --global user.name "GitHub Actions"
 git config --global user.email "actions@github.com"
 git commit -m "Auto update ($(date +'%Y-%m-%d %H:%M:%S'))"
-#git push
+git push
