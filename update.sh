c24e1fdc03d1b17f9ee89dad3b13822546149e8d
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2025-08-26 13:57:22 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
 cd "$GITHUB_WORKSPACE/Named_Snaps"
-for f in *.png; do mv -v -- "$f" "$(echo "$f" | sed -E 's/^(.*\([0-9]{4}\))\..*/\1.png/')"; done
 cd "$GITHUB_WORKSPACE/Named_Titles"
-for f in *.png; do mv -v -- "$f" "$(echo "$f" | sed -E 's/^(.*\([0-9]{4}\))\..*/\1.png/')"; done
 cd "$GITHUB_WORKSPACE/Named_Boxarts"
-for f in *.png; do mv -v -- "$f" "$(echo "$f" | sed -E 's/^(.*\([0-9]{4}\))\..*/\1.png/')"; done
 cd "$GITHUB_WORKSPACE/Named_Logos"
-for f in *.png; do mv -v -- "$f" "$(echo "$f" | sed -E 's/^(.*\([0-9]{4}\))\..*/\1.png/')"; done
 
 cd "$GITHUB_WORKSPACE"
 git add .
