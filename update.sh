f1e165b9178d2f5e2017ce6718dbb2a6dff1a34e
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2025-08-26 14:00:34 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
 cd "$GITHUB_WORKSPACE/Named_Snaps"
+
+for file in "${1:-.}"/*; do
+    [ -f "$file" ] || continue    
+    old=$(basename "$file")
+    new="$old"
+    new=$(echo "$new" | sed 's/\[[^]]*\]//g; s/{[^}]*}//g')
+    new=$(echo "$new" | sed 's/(\([^)]*\))/###KEEP###\1###KEEP###/; s/([^)]*)//g; s/###KEEP###/(/g; s/###KEEP###/)/g')
+    new=$(echo "$new" | sed 's/  */ /g; s/^ *//; s/ *$//')
+    [ "$old" != "$new" ] && [ ! -e "$(dirname "$file")/$new" ] && mv "$file" "$(dirname "$file")/$new" && echo "$old -> $new"
+done
+
 cd "$GITHUB_WORKSPACE/Named_Titles"
 cd "$GITHUB_WORKSPACE/Named_Boxarts"
 cd "$GITHUB_WORKSPACE/Named_Logos"
@@ -10,4 +21,4 @@ git add .
 git config --global user.name "GitHub Actions"
 git config --global user.email "actions@github.com"
 git commit -m "Auto update ($(date +'%Y-%m-%d %H:%M:%S'))"
-git push
+#git push
